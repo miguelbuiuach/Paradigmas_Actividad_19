bb94ecf7ed9cf7ed3f1a76c19ba45fb96ea4c40b
miguel <a320704@uach.mx>
2020-10-30 18:30:57 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({})
+
+app.component('foco',{
+    props: ['lugar'],
+    data() {
+        return {
+            estatus: 0
+        }
+    },
+    template: `
+        <img :src="estatus ? 'images/focoOn.png' : 'images/focoOff.png'"
+        @click="estatus = !estatus">
+        <p>{{lugar}}</p>
+        `
+})
+
+const vm = app.mount('#app')
